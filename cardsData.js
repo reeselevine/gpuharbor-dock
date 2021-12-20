ccb85cc5f545305a7a96571c65563c148c70f1de
Reese Levine <reeselevine1@gmail.com>
2021-12-20 11:14:45 -0800

    Initial landing page

---
+++ cardsData.js
@@ -0,0 +1,10 @@
+let cardsData = [{
+  id: 1,
+  image: 'https://source.unsplash.com/QPNpXcQfdLA/1280x720',
+  info: {
+    path: 'webgpu-mem-testing',
+    title: 'WebGPU Memory Model Testing'
+  },
+  content: 'This project uses small concurrent programs called litmus tests to understand and test the behavior of the WebGPU memory model.'
+}
+]
