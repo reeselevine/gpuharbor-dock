f1db108f9c3e41c88d38308894d822e1eaa77b20
Tyler Sorensen <tyler.sorensen@outlook.com>
2022-08-03 10:13:49 -0700

    added gpu forward progress card

--- cardsData.js
+++ cardsData.js
@@ -1,10 +1,21 @@
-let cardsData = [{
-  id: 1,
-  image: 'https://source.unsplash.com/QPNpXcQfdLA/1280x720',
-  info: {
-    path: 'webgpu-mem-testing',
-    title: 'WebGPU Memory Model Testing'
-  },
-  content: 'This project uses small concurrent programs called litmus tests to understand and test the behavior of the WebGPU memory model.'
-}
+let cardsData = [
+    {
+	id: 1,
+	image: 'https://source.unsplash.com/QPNpXcQfdLA/1280x720',
+	info: {
+	    path: 'webgpu-mem-testing',
+	    title: 'WebGPU Memory Model Testing'
+	},
+	content: 'This project uses small concurrent programs called litmus tests to understand and test the behavior of the WebGPU memory model.'
+    },
+    {
+	id: 2,
+	image: 'tidepools.png',
+	info: {
+	    path: 'gpu-forward-progress',
+	    title: 'GPU Forward Progress Testing'
+	},
+	content: 'A vizualization of the GPU forward progress tests generated for the OOPSLA 2021 paper: Specifying and Testing GPU Workgroup Progress Models.'
+    }
+
 ]
